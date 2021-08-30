e5a3f8537b09c6dae5f139c2ed8c06dcf65419b6
Dušan Simić <dusan.simic1810@gmail.com>
2021-08-31 00:59:11 +0200

    Make major updates to the theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,10 @@
+const round = (num) =>
+	num
+		.toFixed(7)
+		.replace(/(\.[0-9]+?)0+$/, '$1')
+		.replace(/\.0$/, '');
+const em = (px, base) => `${round(px/base)}em`;
+
 module.exports = {
 	purge: {
 		enabled: true,
@@ -17,6 +24,25 @@ module.exports = {
 			sans: ['Ubuntu', 'sans']
 		},
 		extend: {
+			backgroundImage: () => ({
+				'blob-pattern': "url('/svg/blob.svg')",
+			}),
+			boxShadow: {
+				pill: '0 0 0 0 rgba(59, 130, 246, 0.3)',
+				'pill-lg': '0 0 0 4px rgba(59, 130, 246, 0.3)'
+			},
+			transitionDuration: {
+				35: '35ms'
+			},
+			transitionProperty: {
+				'box-shadow': 'box-shadow'
+			},
+			width: {
+				prose: '65ch'
+			},
+			minHeight: {
+				96: '24rem'
+			},
 			colors: {
 				blurk: {
 					300: '#202938',
@@ -24,29 +50,37 @@ module.exports = {
 					DEFAULT: '#0d1117',
 					600: '#080e17'
 				},
+				link: {
+					bg: 'rgba(59, 130, 246, 0.3)',
+				},
 				border: {
 					light: '#e1e4e8',
 					dark: '#30363d'
 				},
-				link: {
-					light: '#0366d6',
-					DEFAULT: '#0366d6',
-					dark: '#58a6ff'
-				}
+				gray: {
+					'100': '#F8F8F8',
+					'200': '#E0E0E0',
+					'300': '#C8C8C8',
+					'400': '#888888',
+					'500': '#707070',
+					'600': '#505050',
+					'700': '#383838',
+					'800': '#282828',
+					'900': '#101010',
+				},
 			},
-			typography: (theme) => ({
+			typography: theme => ({
 				DEFAULT: {
 					css: {
 						a: {
-							color: theme('colors.current'),
-							'&:hover': {
-								color: theme('colors.link.DEFAULT')
-							}
+							color: theme('colors.color.600'),
+							textDecoration: 'none'
 						},
 						h1: {
 							color: theme('colors.current')
 						},
 						h2: {
+							marginTop: '1rem',
 							color: theme('colors.current')
 						},
 						h3: {
@@ -62,9 +96,29 @@ module.exports = {
 					css: {
 						color: theme('colors.gray.50'),
 						a: {
-							color: theme('colors.current'),
+							color: theme('colors.blue.500'),
+							transitionProperty: theme('transitionProperty.box-shadow'),
+							transitionTimingFunction: theme('transitionTimingFunction.ease-in-out'),
+							transitionDuration: theme('transitionDuration.35'),
+							backgroundColor: theme('backgroundColor.link.bg'),
+							borderRadius: theme('borderRadius.md'),
+							textDecoration: theme('textDecoration.no-underline'),
+							padding: '0.25rem 0.5rem 0.25rem 0.5rem',
+							boxShadow: theme('boxShadow.pill'),
 							'&:hover': {
-								color: theme('colors.link.dark')
+								boxShadow: theme('boxShadow.pill-lg')
+							},
+							'&[class~=link-button]': {
+								borderRadius: theme('borderRadius.full'),
+								padding: '0.75rem 1rem 0.75rem 1rem'
+							},
+							'&[class~=link-title]': {
+								color: theme('colors.current'),
+								transitionProperty: theme('transitionProperty.none'),
+								backgroundColor: theme('backgroundColor.current'),
+								borderRadius: theme('borderRadius.none'),
+								padding: '0 0 0 0',
+								boxShadow: theme('boxShadow.none')
 							}
 						},
 						h1: {
@@ -72,6 +126,9 @@ module.exports = {
 						},
 						h2: {
 							color: theme('colors.current')
+						},
+						'.nextprev h2': {
+							fontWeight: 'bold'
 						},
 						h3: {
 							color: theme('colors.current')
